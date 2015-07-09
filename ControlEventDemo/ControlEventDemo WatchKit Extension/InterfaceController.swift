ef86c937034569d1351b61041ffefcd2e87b7100
Jim Veneskey <github@strifecrafter.com>
2015-07-09 12:40:25 -0400

    This was a very brief outline demo showing the execution order of the watchkit app

---
+++ ControlEventDemo/ControlEventDemo WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,33 @@
+//
+//  InterfaceController.swift
+//  ControlEventDemo WatchKit Extension
+//
+//  Created by jim Veneskey on 7/9/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    // These functions illustrate the watch application life cycle
+    
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
