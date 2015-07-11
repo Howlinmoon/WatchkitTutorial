43e8bc31a43a0b0d2d85ed9a67f687a6209afb71
Jim Veneskey <github@strifecrafter.com>
2015-07-11 08:37:22 -0400

    Added a supporting class for the table rows - still no content in the row(s) yet

---
+++ TableDemo/TableDemo WatchKit Extension/MovieRowController.swift
@@ -0,0 +1,18 @@
+//
+//  MovieRowController.swift
+//  TableDemo
+//
+//  Created by Jim Veneskey on 7/11/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+import UIKit
+import WatchKit
+
+class MovieRowController: NSObject {
+   
+    @IBOutlet weak var movieTitle: WKInterfaceLabel!
+
+    @IBOutlet weak var subtitle: WKInterfaceLabel!
+
+}
